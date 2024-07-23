f6d1c3d7d631276af60592b4985e3939a04a688c
Ynvers <luzolo.adoho@gmail.com>
2024-07-23 21:44:59 +0100

    Génération du premier prompt

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ import os
 from dotenv import load_dotenv
 from pypdf import PdfReader
 from langchain.text_splitter  import CharacterTextSplitter
-from langchzin.enbeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from htmlTemplates import css, bot_template, user_template
+
+
 #Charger les vairables d'environnement
 load_dotenv()
-genai_api_key = os.getenv('GENAI_API_KEY')
+genai_api_key = os.getenv('GOOGLE_API_KEY')
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -27,17 +33,60 @@ def get_text_chunks(texte_brute):
     chunks = text_splitter.split_text(texte_brute)
     return chunks
 
-def get_vector_store(text_chnuks):
-    embeddings = OpenAIEmbeddings()
-    vectorestore = FAISS.from_texts(texts=text_chnuks, embedding=embeddings)
+def get_vector_store(text_chunks):
+    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5", encode_kwargs={"normalize_embeddings": True})
+    vectorestore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorestore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-1.5-pro", 
+        temperature=0
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_message=True)
+    
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        retriever= vectorstore.as_retriever(),
+        memory= memory
+    )
+
+    if conversation_chain is None:
+        print("Conversation chain is None")
+    else:
+        print("Conversation chain initialized successfully")
+
+    return conversation_chain
+
+def handle_userinput(user_question):
+    if st.session_state.conversation:
+        response = st.session_state.conversation({"question": user_question})
+        if 'answer' in response:
+            st.write(response['answer'])
+        else:
+            st.write(response)
+    else:
+        st.warning("Conversation is not initialized.")
+
+
 
 def main():
     #Configuration de la page streamlit
     st.set_page_config(page_title="Chat with multiple PDF's", page_icon=":books:")
+    
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
 
     st.header("Welcome to the Multi-PDF :books: Chat Application")
-    st.text_input("Ask a question about your documents:")
+    user_question = st.text_input("Ask a question about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
+    st.write(user_template.replace("{{MSG}}", "hello bot"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "hello user"), unsafe_allow_html=True)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -59,9 +108,14 @@ def main():
                     text_chunks = get_text_chunks(texte_brute)
                     
                     #Créer le store de vecteur
-                    vec
+                    vectorstore = get_vector_store(text_chunks)
+                    
+                    #Créer la haine de conversation
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+
             else:
                 st.warning("Please upload at least one PDF file.")
+        
 
 if __name__ == '__main__':
     main()
